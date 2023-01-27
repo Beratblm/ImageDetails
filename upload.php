a6026117e9272f2fec1d66ec471870242eaec722
Berat <60112637+Beratblm@users.noreply.github.com>
2023-01-27 22:11:05 +0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -4,26 +4,19 @@ require_once("file.php");
 
 
 if (!isset($_SERVER['HTTP_REFERER'])) {
-
     header('location: test.php');
     exit;
 }
 
 if (isset($_FILES["files"])) {
-
     $file = new File($_FILES["files"]["name"], $_FILES["files"]["size"], $_FILES["files"]["type"], $_FILES["files"]["error"], $_FILES["files"]["tmp_name"], $_FILES["files"]["tmp_name"]);
-   
-   $fileexif =  $file->getFile();
+    $fileexif =  $file->getFile();
    
 
 
 }
 
-
-
-
 ?>
-
 
 <!DOCTYPE html>
 <html lang="en">
@@ -76,5 +69,4 @@ if (isset($_FILES["files"])) {
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
 </body>
-
 </html>
